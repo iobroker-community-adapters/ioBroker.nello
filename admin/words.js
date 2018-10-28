a6930dddc31469f57fa9ccb4cce5b003ed1aeb7d
Zefau <Zefau@users.noreply.github.com>
2018-10-28 21:03:11 +0100

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	"auth": {
+		"en": "Authentication Credentials",
+		"de": "Authentifizierung"
+	},
+	"auth_info": {
+		"en": "The nello auth API is responsible for authentication of all nello client apps. This service follows OAuth2 as an authentication scheme to authenticate an app/user. For further information about the OAuth2 standard, please check here: <a href='https://oauth.net/2/'>https://oauth.net/2/</a><br /> To use this service, client credentials must be obtained from the nello auth admin UI located at: <a href='https://auth.nello.io/admin'>https://auth.nello.io/admin</a>. Please not that for the time being you can only get one pair of client_id and client_secret. They consist of a client_id and a client_secret.",
+		"de": ""
+	},
+    "main": {
+		"en": "Main Settings",
+		"de": "Einstellungen"
+	},
+    "client_id": {
+		"en": "Client ID",
+		"de": "Client ID"
+	},
+    "client_secret": {
+		"en": "Client Secret",
+		"de": "Client Secret"
+	},
+    "token_type": {
+		"en": "Token Type (Readonly)",
+		"de": "Token Typ (nur lesend)"
+	},
+    "access_token": {
+		"en": "Access Token (Readonly)",
+		"de": "Access Token (nur lesend)"
+	},
+	"token_info": {
+		"en": "Fill in both Client ID / Client Secret in order to generate token.",
+		"de": "Trage Client ID / Client Secret ein, um einen neuen Token zu generieren."
+	},
+	"button_token": {
+		"en": "Get token",
+		"de": "Token generieren"
+	}
+};
